d5552ee3f66693a409a839a94653ebcb292af8b7
tharakaprabash <tplstlsml@gmail.com>
2026-08-23 17:28:47 +0530

    SOL-98 Consumption

--- src/components/dashboard/ConsumptionView.js
+++ src/components/dashboard/ConsumptionView.js
@@ -4,6 +4,7 @@ import { useEnergy } from '../../context/EnergyContext';
 import { COLORS, GLASS, SHADOWS } from '../../theme/colors';
 import { Zap, Wind, Repeat, Droplet, Box, Tv, AlertTriangle, Sparkles, Clock } from 'lucide-react-native';
 
+// SOL-98: Consumption View Component
 export const ConsumptionView = () => {
   const { metrics, appliances, toggleAppliance } = useEnergy();
 
